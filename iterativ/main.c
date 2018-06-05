fe483854506e2b4f583e5515aec4c581150b864a
AnamariaL <lilea.anamaria@yahoo.com>
2018-06-05 11:25:14 +0300

    add main
    iterativ algorithm for MovingCake

--- iterativ/main.c
+++ iterativ/main.c
@@ -1,192 +1,153 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int p1[100];
-int p2[100];
-int p3[100];
-int top1=0;
-int top2=0;
-int top3=0;
-
- void pushp1(int a)
+// A structure to represent a stack
+struct Stack
 {
-	if(a!=0)
-	p1[top1++]=a;
+   unsigned capacity;
+   int top;
+   int *array;
+};
+
+// function to create a stack of given capacity.
+struct Stack* createStack(unsigned capacity)
+{
+    struct Stack* stack =
+        (struct Stack*) malloc(sizeof(struct Stack));
+    stack -> capacity = capacity;
+    stack -> top = -1;
+    stack -> array =
+        (int*) malloc(stack -> capacity * sizeof(int));
+    return stack;
 }
 
-int popp1()
+// Stack is full when top is equal to the last index
+int isFull(struct Stack* stack)
 {
-	if(top1==0)
-		return 0;
-	else
-		return p1[--top1];
+   return (stack->top == stack->capacity - 1);
 }
 
-void displayp1()
+// Stack is empty when top is equal to -1
+int isEmpty(struct Stack* stack)
 {
-	int i;
-	printf("\nSilver Plate ->> ");
-	if(top1==0)
-		printf("Empty");
-	else
-	for(i=0;i<top1;i++)
-	{
-		printf("%d ",p1[i]);
-	}
-	printf("\n");
+   return (stack->top == -1);
 }
 
-void pushp2(int a)
+// Function to add an item to stack.  It increases
+// top by 1
+void push(struct Stack *stack, int item)
 {
-	if(a!=0)
-	p2[top2++]=a;
-}
-int popp2()
-{
-	if(top2==0)
-		return 0;
-	else
-		return p2[--top2];
+    if (isFull(stack))
+        return;
+    stack -> array[++stack -> top] = item;
 }
 
-void displayp2()
+// Function to remove an item from stack.  It
+// decreases top by 1
+int pop(struct Stack* stack)
 {
-	int i;
-	printf("Bronze Plate->> ");
-	if(top2==0)
-		printf("Empty");
-	else
-	for(i=0;i<top2;i++)
-	{
-		printf("%d ",p2[i]);
-	}
-	printf("\n");
+    if (isEmpty(stack))
+        return INT_MIN;
+    return stack -> array[stack -> top--];
 }
 
-void pushp3(int a)
+// Function to implement legal movement between
+// two poles
+void moveDisksBetweenTwoPoles(struct Stack *src,
+            struct Stack *dest, char s, char d)
 {
-	if(a!=0)
-	p3[top3++]=a;
-}
-int popp3(void)
-{
-	if(top3==0)
-		return 0;
-	else
-		return p3[--top3];
+    int pole1TopDisk = pop(src);
+    int pole2TopDisk = pop(dest);
+
+    // When pole 1 is empty
+    if (pole1TopDisk == INT_MIN)
+    {
+        push(src, pole2TopDisk);
+        moveDisk(d, s, pole2TopDisk);
+    }
+
+    // When pole2 pole is empty
+    else if (pole2TopDisk == INT_MIN)
+    {
+        push(dest, pole1TopDisk);
+        moveDisk(s, d, pole1TopDisk);
+    }
+
+    // When top disk of pole1 > top disk of pole2
+    else if (pole1TopDisk > pole2TopDisk)
+    {
+        push(src, pole1TopDisk);
+        push(src, pole2TopDisk);
+        moveDisk(d, s, pole2TopDisk);
+    }
+
+    // When top disk of pole1 < top disk of pole2
+    else
+    {
+        push(dest, pole2TopDisk);
+        push(dest, pole1TopDisk);
+        moveDisk(s, d, pole1TopDisk);
+    }
 }
 
-void displayp3()
+//Function to show the movement of disks
+void moveDisk(char fromPeg, char toPeg, int disk)
 {
-	int i;
-	printf("Golden Plate ->> ");
-	if(top3==0)
-		printf("Empty");
-	else
-	for(i=0;i<top3;i++)
-	{
-		printf("%d ",p3[i]);
-	}
-	printf("\n");
+    printf("Move the disk %d from \'%c\' to \'%c\'\n",
+           disk, fromPeg, toPeg);
 }
 
+//Function to implement TOH puzzle
+void tohIterative(int num_of_disks, struct Stack
+             *src, struct Stack *aux,
+             struct Stack *dest)
+{
+    int i, total_num_of_moves;
+    char s = 'S', d = 'D', a = 'A';
+
+    //If number of disks is even, then interchange
+    //destination pole and auxiliary pole
+    if (num_of_disks % 2 == 0)
+    {
+        char temp = d;
+        d = a;
+        a  = temp;
+    }
+    total_num_of_moves = pow(2, num_of_disks) - 1;
+
+    //Larger disks will be pushed first
+    for (i = num_of_disks; i >= 1; i--)
+        push(src, i);
+
+    for (i = 1; i <= total_num_of_moves; i++)
+    {
+        if (i % 3 == 1)
+          moveDisksBetweenTwoPoles(src, dest, s, d);
+
+        else if (i % 3 == 2)
+          moveDisksBetweenTwoPoles(src, aux, s, a);
+
+        else if (i % 3 == 0)
+          moveDisksBetweenTwoPoles(aux, dest, a, d);
+    }
+}
+
+// Driver Program
 int main()
 {
-	int n,i,x,a,b;
-	printf("\nEnter the no. of stories: ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-	    pushp1(n-i);
-	}
-	x=pow(2,n)-1;
-	displayp1();
-	displayp2();
-	displayp3();
-	for(i=1;i<=x;i++)
-	{
-		if(i%3==1)
-		{
-	    	a=popp1();
-	 	    b=popp3();
-		    if(a==0)
-			{
-				pushp1(b);
-			}
-			else if(b==0)
-			{
-			    pushp3(a);
-			}
-			else if(a>b)
-			{
-				pushp1(a);
-				pushp1(b);
-			}
-			else
-			{
-				pushp3(b);
-				pushp3(a);
-			}
-			displayp1();
-			displayp2();
-			displayp3();
-			continue;
-		}
-        else if(i%3==2)
-		{
-		a=popp1();
-		b=popp2();
-		if(a==0)
-        {
-            pushp1(b);
-        }
-        else if(b==0)
-        pushp2(a);
-        else if(a>b)
-        {
-            pushp1(a);
-            pushp1(b);
-        }
-        else
-        {
-            pushp2(b);
-            pushp2(a);
-        }
-        displayp1();
-        displayp2();
-        displayp3();
-        continue;
-    }
-    else if(i%3==0)
-    {
-		a=popp2();
-		b=popp3();
-		if(a==0)
-        {
-            pushp2(b);
-        }
-        else if(b==0)
-            pushp3(a);
-        else if(a>b)
-        {
-            pushp2(a);
-            pushp2(b);
-        }
-        else
-        {
-            pushp3(b);
-            pushp3(a);
-        }
-        displayp1();
-        displayp2();
-        displayp3();
-        continue;
-    }
-  }
-<<<<<<< HEAD
-=======
+    // Input: number of disks
+    unsigned num_of_disks = 3;
 
->>>>>>> aaca3aa20228a841d7d5cabaf2ebee167199824d
-  return 0;
+    struct Stack *src, *dest, *aux;
+
+    // Create three stacks of size 'num_of_disks'
+    // to hold the disks
+    src = createStack(num_of_disks);
+    aux = createStack(num_of_disks);
+    dest = createStack(num_of_disks);
+
+    tohIterative(num_of_disks, src, aux, dest);
+    return 0;
 }
