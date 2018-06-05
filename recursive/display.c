5a1c633091f72c3eee270626c13e16ea552f65e3
AnamariaL <lilea.anamaria@yahoo.com>
2018-06-05 22:36:18 +0300

    add display

---
+++ recursive/display.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int display(int n,char source,char destination){
+ printf("\nMove layer %d from  %c plate to  %c plate",n,source,destination);
+}
